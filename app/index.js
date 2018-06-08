aa8dd4445aa675a5e59270257905fe37111d8c6b
郝海亮 <2298763729@qq.com>
2018-06-08 16:23:13 +0800

    基本框架搭建起来了

--- app/index.js
+++ app/index.js
@@ -1,4 +1,9 @@
-import React from 'react';
-import './index.less';
+import React      from 'react';
+import { render } from 'react-dom';
+import Hello      from './components/hello';
 
 console.log( React.version );
+render(
+	<Hello></Hello>,
+	document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,40 @@
+'use strict';
+
+var path              = require('path');
+var webpack           = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-	entry:'./app/index.js',
-	output:{
-		path:'./dist',
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
+    devtool: 'eval-source-map',
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        'react-hot-loader/patch',
+        path.join(__dirname, 'app/index.js')
+    ],
+    output: {
+        path: path.join(__dirname, '/dist/'),
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './app/index.tpl.html',
+          inject: 'body',
+          filename: './index.html'
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ],
+    module: {
+        resolve:{
+            extensions:['','.js','.json']
+        },
+        loaders: [
             {
               test: /\.js$/,
               exclude: /node_modules/,
@@ -28,5 +57,5 @@ module.exports = {
                 loader: 'style-loader!css-loader!less-loader'
             }
         ]
-	}
+    }
 };
