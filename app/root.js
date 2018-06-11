4a6bad5ccea0d73784ca5d2c49b8f373ad44a72c
郝海亮 <2298763729@qq.com>
2018-06-11 21:54:10 +0800

    add route

--- app/root.js
+++ app/root.js
@@ -1,10 +1,11 @@
-import React          from 'react';
-import Header         from './components/header';
-import Player         from './page/player';
-import MusicList      from './page/musiclist';
-import { MUSIC_LIST } from './config/musiclist';
+import React                                                           from 'react';
+import { Router, IndexRoute, Link, Route, browserHistory, hashHistory} from 'react-router';
+import Header                                                          from './components/header';
+import Player                                                          from './page/player';
+import MusicList                                                       from './page/musiclist';
+import { MUSIC_LIST }                                                  from './config/musiclist';
 
-let Root = React.createClass({
+let App = React.createClass({
 	getInitialState(){
 		return {
 			musicList        : MUSIC_LIST,
@@ -14,14 +15,27 @@ let Root = React.createClass({
 	render(){
 		return (
 			<div>
-				<div id="player"></div>
 				<Header></Header>
+				{ React.cloneElement(this.props.children, this.state) }
 				<MusicList
 					currentMusicItem={ this.state.currentMusicItem }
 					musicList={ this.state.musicList }>
 				</MusicList>
 			</div>
 		)
+	}
+});
+
+let Root = React.createClass({
+	render() {
+	    return (
+		    <Router history={hashHistory}>
+		        <Route path="/" component={App}>
+		            <IndexRoute component={Player}/>
+		            <Route path="/list" component={MusicList} />
+		        </Route>
+		    </Router>
+		);
 	}
 });
 
