cd474a07ea91fdb1220103d46aef20cc6df1af47
郝海亮 <2298763729@qq.com>
2018-06-11 21:32:20 +0800

    add 当前项目高亮选中

--- app/components/musiclistitem.js
+++ app/components/musiclistitem.js
@@ -5,7 +5,7 @@ let MusicListItem = React.createClass({
 	render(){
 		let musicItem = this.props.musicItem;
 		return (
-			<li className="components-listitem row">
+			<li className={`components-listitem row${this.props.focus ? ' focus' : ''}`}>
 				<p><strong>{ musicItem.title }</strong> - { musicItem.artist }</p>
 				<p className="-col-auto delete"></p>
 			</li>
