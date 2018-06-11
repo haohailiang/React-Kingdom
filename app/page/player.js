b82585a73f99d27559888d9b615190931c12a5c2
郝海亮 <2298763729@qq.com>
2018-06-11 16:51:30 +0800

    add 播放器静态皮肤建立

--- app/page/player.js
+++ app/page/player.js
@@ -38,12 +38,42 @@ let Player = React.createClass({
 	render(){
 		return (
 			<div>
-				<div id="player"></div>
-				<Process
-					progress={ this.state.progress }
-					onProgressChange={ this.progressChangeHandler }
-					barColor="#FF0000">
-				</Process>
+				<div className="player-page">
+	                <h1 className="caption">我的私人音乐坊 &gt;</h1>
+	                <div className="mt20 row">
+	                	<div className="controll-wrapper">
+	                		<h2 className="music-title">歌曲名称</h2>
+	                		<h3 className="music-artist mt10">歌手</h3>
+	                		<div className="row mt20">
+	                			<div className="left-time -col-auto">-2:00</div>
+	                			<div className="volume-container">
+	                				<i className="icon-volume rt" style={{top: 5, left: -5}}></i>
+	                				<div className="volume-wrapper">
+										音量控制部分
+									</div>
+								</div>
+							</div>
+							<div style={{height: 10, lineHeight: '10px'}}>
+								播放进度部分
+								{/* <Process
+									progress={ this.state.progress }
+									onProgressChange={ this.progressChangeHandler }
+									barColor="#FF0000">
+								</Process> */}
+							</div>
+							<div className="mt35 row">
+                			<div>
+	                			<i className="icon prev"></i>
+	                			<i className={`icon ml20 play`}></i>
+	                			<i className="icon next ml20"></i>
+                			</div>
+                			<div className="-col-auto">
+                				<i className={`icon repeat-cycle`}></i>
+                			</div>
+                		</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
