49a6073265b30f35f63622160cccc1c37549e248
郝海亮 <2298763729@qq.com>
2018-06-11 15:44:57 +0800

    点击计算进度条

--- app/components/progress.js
+++ app/components/progress.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './progress.less';
 
 let Progress = React.createClass({
+	changeProgress(e){
+		let progressBar = this.refs.progressBar;
+		let progress = (e.clientX - progressBar.getBoundingClientRect().left) / progressBar.clientWidth;
+		console.log( "  e.clientX: "+ e.clientX + "  progressBar.getBoundingClientRect: " + progressBar.getBoundingClientRect().left + "  progressBar.clientWidth: " + progressBar.clientWidth + "  progressBar.clientHeight: " + progressBar.clientHeight  );
+		console.log( progress );
+		// this.props.onProgressChange && this.props.onProgressChange(progress);
+	},
 	render(){
 		return (
-			<div className="components-progress row">
-				<div className="progress" style={{ width:`${this.props.progress}%` }}>
-					{ this.props.progress }S
-				</div>
+			<div className="components-progress row" ref="progressBar" onClick={ this.changeProgress }>
+				<div className="progress" style={{ width:`${this.props.progress}%` }}></div>
 			</div>
 		)
 	}
