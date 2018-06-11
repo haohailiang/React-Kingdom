df2282d384f6a519a5433d37791a01f2de2dac93
郝海亮 <2298763729@qq.com>
2018-06-11 14:52:20 +0800

    音乐通了,不错哦~

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 课程名称:React音乐播放器
 * 特点
 * 工程化构建React项目
+[jplayer官网](http://www.jplayer.org/)
+# 生命周期
+* getDefaultProps
+* getInitialState
+* componentWillMount
+* render
+
+* componentDidMount
+* componentWillUnmount
+* componentDidUpdate
+* shouldComponentUpdate
+* componentWillReceiveProps
+* componentWillUpdate
+* componentDidUpdate
--- app/root.js
+++ app/root.js
@@ -1,14 +1,37 @@
 import React   from 'react';
 import Header  from './components/header';
-import Process from './components/process';
+import Process from './components/progress';
 
 let Root = React.createClass({
+	getInitialState(){
+		return {
+			progress: '0'
+		}
+	},
+	componentDidMount(){
+		$("#player").jPlayer({
+			ready: function () {
+				$(this).jPlayer("setMedia", {
+					mp3: "http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3"
+				}).jPlayer('play');
+			},
+			supplied: "mp3",
+			wmode: "window",
+			useStateClassSkin: true
+		});
+		$("#player").bind($.jPlayer.event.timeupdate, (e) => {
+			this.setState({
+				progress: Math.round(e.jPlayer.status.currentTime)
+			});
+		});
+	},
 	render(){
 		return (
 			<div>
+				<div id="player"></div>
 				<Header></Header>
 				<Process
-					process="1">
+					progress={ this.state.progress }>
 				</Process>
 			</div>
 		)
