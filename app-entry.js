98775b208cae45e56fd3105a12ecb0c7d5ea2307
郝海亮 <2298763729@qq.com>
2018-06-17 12:56:39 +0800

    生成随机颜色的圆

--- app-entry.js
+++ app-entry.js
@@ -2,8 +2,18 @@ import React      from 'react'
 import { render } from 'react-dom'
 import Circle     from './components/Circle'
 
+var theCircle = <Circle bgColor="#F9D240"></Circle>;
+
+function showCircle() {
+	var colors = ["#393E41", "#E94F37", "#1C89BF", "#A1D363"];
+
+	var ran = Math.floor(Math.random() * colors.length);
+
+	return <Circle bgColor={ colors[ran] }></Circle>
+}
+
 render((
 	<div>
-		<Circle bgColor="#F9D240"></Circle>
+		{ showCircle() }
 	</div>
 ), document.getElementById('container'))
